5a6dac53b981e55b0a55f953f42098efd6177f9c
danyelmurray1987@gmail.com <Guest@Epicodus-5C.local>
2018-01-26 09:22:09 -0800

    add pakage, web search to find information to incluse in web pages. map app routes.

--- src/main/java/models/ArchaeaPhylum.java
+++ src/main/java/models/ArchaeaPhylum.java
@@ -3,6 +3,10 @@ package models;
 
 import models.*;
 
+
+//Euryarchaeota and Crenarchaeota.
+
+
 public class ArchaeaPhylum extends ArchaeaDomain {
     public String phylumName;
     public int phylumId;
@@ -28,5 +32,25 @@ public class ArchaeaPhylum extends ArchaeaDomain {
 
     public void setPhylumId(int phylumId) {
         this.phylumId = phylumId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ArchaeaPhylum that = (ArchaeaPhylum) o;
+
+        if (phylumId != that.phylumId) return false;
+        return phylumName.equals(that.phylumName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + phylumName.hashCode();
+        result = 31 * result + phylumId;
+        return result;
     }
 }
